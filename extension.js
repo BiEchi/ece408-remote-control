100e33af5e909011067f552c908e80e6326d6515
Jackgetup <Haob.19@intl.zju.edu.cn>
2021-10-10 00:21:54 +0800

    finished the production version

--- extension.js
+++ extension.js
@@ -10,20 +10,28 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-		// This part of code will only be executed once when your extension is activated
-		var account = "haob2"; 
-		var passwd = "thanbell16";
-		var num_lab = 1; // interger
+	// ********** GLOBAL-PARAMETERS ********** //
 
-		// Initialization
-		const {Builder, By, Key, until} = require('selenium-webdriver');
-		var webdriver = require('selenium-webdriver');
-	
-		var driver = new webdriver.Builder()
-			.forBrowser('chrome')
-			.build();
-		// switch to full screen
-		driver.manage().window().maximize(); 
+	// This part of code will only be executed once when your extension is activated
+	var account = 'haob2'; 
+	var passwd = 'thanbell16';
+	var num_lab = 2; // -1 for default
+	// lookup table for the #lab-addr map
+	var addr_list = ['9999', '10001', '10002', '10003', '10010', '10004', '10005', '10011', '10124'];
+	var addr = addr_list[num_lab];
+	var first_time_login = true;
+
+	// Initialization
+	const {By, Key, until} = require('selenium-webdriver');
+	var webdriver = require('selenium-webdriver');
+	var driver = new webdriver.Builder()
+		.forBrowser('chrome')
+		.build();
+
+	// switch to full screen (only once)
+	driver.manage().window().maximize(); 
+
+	// ********** HELPER-FUNCTIONS ********** //
 
 	function bad_ssl(driver) {
 		// website error handling (temporary)
@@ -35,32 +43,35 @@ function activate(context) {
 	}
 
 	function login(driver) {
-		var login_button = driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/div/div/div[3]/a[2]/div'), 20));
-		login_button.click();
-
-		var account_box = driver.wait(until.elementLocated(By.xpath('//*[@id="user_name"]'), 20));
-		account_box.sendKeys(account);
-		var passwd_box = driver.wait(until.elementLocated(By.xpath('//*[@id="password"]'), 20));
-		passwd_box.sendKeys(passwd);
-
-		var confirm_login_button = driver.wait(until.elementLocated(By.xpath('/html/body/div/div/div/form/div[3]/div/button'), 20));
-		confirm_login_button.click();
-
-		// optional, only for narrow size pattern
-		var dropdown_toggle = driver.wait(until.elementLocated(By.xpath('/html/body/nav/div/div/button'), 20));
-		dropdown_toggle.click();
-
-		// Go to the number of lab you want
-		var lab_toggle = driver.wait(until.elementLocated(By.xpath('/html/body/nav/div/nav/ul[1]/li[1]/a'), 20));
-		lab_toggle.click();
-
-		var xpath_num = num_lab + 4;
-		var xpath_lab_button = '/html/body/nav/div/nav/ul[1]/li[1]/ul/li[' + xpath_num.toString() + ']/a';
-		var lab_button = driver.wait(until.elementLocated(By.xpath(xpath_lab_button), 20));
-		lab_button.click();
-
-		var code_tab = driver.wait(until.elementLocated(By.xpath('//*[@id="code-tab"]'), 20));
-		code_tab.click();
+		if (first_time_login == true)
+		{
+			var login_button = driver.wait(until.elementLocated(By.xpath('//*[@id="content"]/div/div/div/div[3]/a[2]/div'), 20));
+			login_button.click();
+	
+			var account_box = driver.wait(until.elementLocated(By.xpath('//*[@id="user_name"]'), 20));
+			account_box.sendKeys(account);
+			var passwd_box = driver.wait(until.elementLocated(By.xpath('//*[@id="password"]'), 20));
+			passwd_box.sendKeys(passwd);
+	
+			var confirm_login_button = driver.wait(until.elementLocated(By.xpath('/html/body/div/div/div/form/div[3]/div/button'), 20));
+			confirm_login_button.click()
+			.then(function(){
+				driver.get('https://www.webgpu.net/mp/' + addr);
+				var code_tab = driver.wait(until.elementLocated(By.xpath('//*[@id="code-tab"]'), 20));
+				code_tab.click();
+			})
+		} else {
+			driver.wait(function() {
+				return driver.findElement(By.xpath('//*[@id="content"]/div/div')).isDisplayed();
+			}, 20)
+			.then(function() {
+				// Go to the number of lab you want
+				driver.get('https://www.webgpu.net/mp/' + addr);
+				var code_tab = driver.wait(until.elementLocated(By.xpath('//*[@id="code-tab"]'), 20));
+				code_tab.click();
+			})
+		}
+		first_time_login = false;
 	}
 
 	function save_file(code){
@@ -77,43 +88,34 @@ function activate(context) {
 		return code;
 	}
 
-	function push_code(code){
-		// call the python script to paste the content
-		const spawn = require("child_process").spawn;
+	function sleep(delay) {
+		var start = (new Date()).getTime();
+		while ((new Date()).getTime() - start < delay) {
+			// 使用  continue 实现；
+			continue; 
+		}
+	}
 
-		// copy the variable onto the clipboard
-		const python_copy = spawn('python',["./copy.py", code]);
-		python_copy.stdout.on('data', (data) => {
-			if (data == 'success') vscode.window.showInformationMessage("Python copy returns value " + data);
-			else vscode.window.showInformationMessage("Python copy returns value " + 'fail');
-		});
-
-		// Move the cursor to the element and click 
+	function push_code_and_run(){
+		// Move the cursor to the first line of code and click 
 		var code_line = driver.wait(until.elementLocated(By.xpath('//*[@id="code"]/div[1]/div[2]/div/span/div/div[6]/div[1]/div/div/div/div[5]/div[1]/pre/span/span'), 20));
-		code_line.click();
-
-		// last test
+		var compile_button = driver.wait(until.elementLocated(By.xpath('//*[@id="code"]/div[1]/div[1]/div/div[2]/div[1]/div'), 20));
+		var all_button = driver.wait(until.elementLocated(By.xpath('/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/ul/li[8]/a'), 20));
 		const actions = driver.actions();
 		actions
 		.click(code_line)
 		.keyDown(Key.COMMAND)
 		.sendKeys('a')
 		.sendKeys('v')
+		// .sendKeys('s')
 		.keyUp(Key.COMMAND)
+		// .sendKeys(code)
+		.click(compile_button)
+		.click(all_button)
 		.perform();
 	}
 
-	function click_run(driver){
-		// click on the "compile & run button" button
-		var compile_button = driver.wait(until.elementLocated(By.xpath('//*[@id="code"]/div[1]/div[1]/div/div[2]/div[1]/div'), 20));
-		compile_button.click();
-		// click on the "all" dataset button
-		var all_button = driver.wait(until.elementLocated(By.xpath('/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/ul/li[8]/a'), 20));
-		all_button.click();
-	}
-
 	function download_html(driver){
-		// driver.get("https://baidu.com"); // test code
 		driver.getCurrentUrl() // nonsense, only for syntax
 			.then(function() {
 				return driver.getCurrentUrl();
@@ -134,12 +136,6 @@ function activate(context) {
 			});
 	}
 
-	function render_html(){
-		// only for testing
-		vscode.window.showInformationMessage("Please use the Live Share extension to render the HTML code!");
-	}
-
-
 	let config_process = vscode.commands.registerCommand('ece408-remote-control.config', function () {
 		vscode.window.showInputBox(
 			{
@@ -155,10 +151,16 @@ function activate(context) {
 		});
 	});
 	
+	// ********** CALLER-FUNCTIONS ********** //
 
 	let login_process = vscode.commands.registerCommand('ece408-remote-control.login', function () {
+		if (num_lab == -1) {
+			vscode.window.showInformationMessage("Please input your account and password first!");
+			return;
+		}
+
 		// Certify the website
-		driver.get('https://www.webgpu.net');
+		driver.get('https://www.webgpu.net/');
 		bad_ssl(driver);
 		login(driver);
 		vscode.window.showInformationMessage("Successfully logged in to your WebGPU account and accessing Lab" + num_lab.toString() + ".");
@@ -166,40 +168,28 @@ function activate(context) {
 
 	let pull_process = vscode.commands.registerCommand('ece408-remote-control.pull', function () {
 		// get the code
-		driver.getCurrentUrl() // nonsense, only for syntax
-		.then(function() {
-			return driver.getCurrentUrl();
-		})
-		.then(function(currentUrl) {
-			// work with the current url of browser
-			var code_editor = driver.wait(until.elementLocated(By.xpath('/html/body/pre'), 20));
-			driver.get(currentUrl + "/program/");
-			return code_editor;
-		})
-		.then(function(code_editor){
+		var code_editor = driver.wait(until.elementLocated(By.xpath('/html/body/pre'), 20));
+		driver.get('https://www.webgpu.net/mp/' + addr + '/program/')
+		.then(function(){
 			var code = code_editor.getText();
-			code.then((code) => {
+			code.then(function(code) {
 				// save the raw data and overwrite the lab project
 				save_file(code);
-				// return to the editing page
-				driver.get('https://www.webgpu.net');
 				// go through the login subroutine again
-				login(driver);
+				driver.get('https://www.webgpu.net/mp/' + addr);
+				var code_tab = driver.wait(until.elementLocated(By.xpath('//*[@id="code-tab"]'), 20));
+				code_tab.click();
 			})
 		})
 	});
 
 	let push_process = vscode.commands.registerCommand('ece408-remote-control.push', function () {
 		// read the file into the buffer
-		var code = read_file();
-		// TODO: transfer the buffer onto the webpage
-		push_code(code);
-		// click on the "submit & run button" and "all" datasets
-		click_run(driver);
-		// pull down the whole HTML source code and save in an HTML file
+		// var code = read_file();
+		// transfer the buffer onto the webpage
+		push_code_and_run();
+		// pull down the whole HTML source code and save in an HTML file (render it yourself)
 		// download_html(driver);
-		// render the HTML source code locally
-		// render_html();
 	});
 
 	let exit_process = vscode.commands.registerCommand('ece408-remote-control.exit', function () {
--- lab2.cu
+++ lab2.cu
@@ -1,73 +1,120 @@
-// MP 1
 #include <wb.h>
 
-__global__ void vecAdd(float *in1, float *in2, float *out, int len) {
-  //@@ Insert code to implement vector addition here
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  if (i < len)
-    out[i] = in1[i] + in2[i];
+#define wbCheck(stmt)                                                     \
+  do {                                                                    \
+    cudaError_t err = stmt;                                               \
+    if (err != cudaSuccess) {                                             \
+      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
+      wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));      \
+      return -1;                                                          \
+    }                                                                     \
+  } while (0)
+
+#define BLOCK_WIDTH 8
+
+
+// Compute C = A * B
+__global__ void matrixMultiply(float *A, float *B, float *C, int numARows,
+                               int numAColumns, int numBRows,
+                               int numBColumns, int numCRows,
+                               int numCColumns) {
+  //@@ Insert code to implement matrix multiplication here
+  int CRow = blockIdx.y * blockDim.y + threadIdx.y;
+  int CColumn = blockIdx.x * blockDim.x + threadIdx.x;
+  
+  if (CRow < numCRows && CColumn < numCColumns)
+  {
+    float CElement = 0;
+    for(int iter = 0; iter < numAColumns; iter++)
+    {
+      float AElement = A[CRow * numAColumns + iter];
+      float BElement = B[iter * numBColumns + CColumn];
+      CElement += AElement * BElement;
+    }
+    C[CRow*numCColumns + CColumn] = CElement;
+    printf("CElement is %f\n", CElement);
+  }
+  return;
 }
 
 int main(int argc, char **argv) {
   wbArg_t args;
-  int inputLength;
-  float *hostInput1;
-  float *hostInput2;
-  float *hostOutput;
-  float *deviceInput1;
-  float *deviceInput2;
-  float *deviceOutput;
+  float *hostA; // The A matrix
+  float *hostB; // The B matrix
+  float *hostC; // The output C matrix
+  float *deviceA;
+  float *deviceB;
+  float *deviceC;
+  int numARows;    // number of rows in the matrix A
+  int numAColumns; // number of columns in the matrix A
+  int numBRows;    // number of rows in the matrix B
+  int numBColumns; // number of columns in the matrix B
+  int numCRows;    // number of rows in the matrix C (you have to set this)
+  int numCColumns; // number of columns in the matrix C (you have to set
+                   // this)
 
   args = wbArg_read(argc, argv);
 
   wbTime_start(Generic, "Importing data and creating memory on host");
-  hostInput1 =
-      (float *)wbImport(wbArg_getInputFile(args, 0), &inputLength);
-  hostInput2 =
-      (float *)wbImport(wbArg_getInputFile(args, 1), &inputLength);
-  hostOutput = (float *)malloc(inputLength * sizeof(float));
+  hostA = (float *)wbImport(wbArg_getInputFile(args, 0), &numARows,
+                            &numAColumns);
+  hostB = (float *)wbImport(wbArg_getInputFile(args, 1), &numBRows,
+                            &numBColumns);
+  //@@ Set numCRows and numCColumns; note that all variables are integers
+  numCRows = numARows;
+  numCColumns = numBColumns;
+  //@@ Allocate the hostC matrix
+  hostC = (float *)malloc(numCRows * numCColumns * sizeof(float));
   wbTime_stop(Generic, "Importing data and creating memory on host");
+
+  // Examination Part
+  wbLog(TRACE, "The dimensions of A are ", numARows, " x ", numAColumns);
+  wbLog(TRACE, "The dimensions of B are ", numBRows, " x ", numBColumns);
 
   wbTime_start(GPU, "Allocating GPU memory.");
   //@@ Allocate GPU memory here
-  int size = inputLength * sizeof(float);
-  cudaMalloc((void **) &deviceInput1, size);
-  cudaMalloc((void **) &deviceInput2, size);
-  cudaMalloc((void **) &deviceOutput, size);
+  int sizeA = numARows * numAColumns * sizeof(float);
+  int sizeB = numBRows * numBColumns * sizeof(float);
+  int sizeC = numCRows * numCColumns * sizeof(float);
+  cudaMalloc((void **) &deviceA, sizeA);
+  cudaMalloc((void **) &deviceB, sizeB);
+  cudaMalloc((void **) &deviceC, sizeC);
   wbTime_stop(GPU, "Allocating GPU memory.");
 
   wbTime_start(GPU, "Copying input memory to the GPU.");
   //@@ Copy memory to the GPU here
-  cudaMemcpy(deviceInput1, hostInput1, size, cudaMemcpyHostToDevice);
-  cudaMemcpy(deviceInput2, hostInput2, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceA, hostA, sizeA, cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceB, hostB, sizeB, cudaMemcpyHostToDevice);
   wbTime_stop(GPU, "Copying input memory to the GPU.");
-  
+
   //@@ Initialize the grid and block dimensions here
-  dim3 DimGrid(ceil(inputLength/256.0), 1, 1);
-  dim3 DimBlock(256, 1, 1);
+  dim3 dimGrid(ceil((1.0*numCColumns)/BLOCK_WIDTH), ceil((1.0*numCRows)/BLOCK_WIDTH), 1);
+  dim3 dimBlock(BLOCK_WIDTH, BLOCK_WIDTH, 1);
   
   wbTime_start(Compute, "Performing CUDA computation");
   //@@ Launch the GPU Kernel here
-  vecAdd<<<DimGrid, DimBlock>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
+  matrixMultiply<<<dimGrid, dimBlock>>>(deviceA, deviceB, deviceC, numARows, numAColumns, 
+                                        numBRows, numBColumns, numCRows, numCColumns);
   cudaDeviceSynchronize();
   wbTime_stop(Compute, "Performing CUDA computation");
-  
+
   wbTime_start(Copy, "Copying output memory to the CPU");
   //@@ Copy the GPU memory back to the CPU here
-  cudaMemcpy(hostOutput, deviceOutput, size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(hostC, deviceC, sizeC, cudaMemcpyDeviceToHost);
   wbTime_stop(Copy, "Copying output memory to the CPU");
-  
+
   wbTime_start(GPU, "Freeing GPU Memory");
   //@@ Free the GPU memory here
-  cudaFree(deviceInput1);
-  cudaFree(deviceInput2);
-  cudaFree(deviceOutput);
+  cudaFree(deviceA);
+  cudaFree(deviceB);
+  cudaFree(deviceC);
   wbTime_stop(GPU, "Freeing GPU Memory");
 
-  wbSolution(args, hostOutput, inputLength);
+  wbSolution(args, hostC, numCRows, numCColumns);
 
-  free(hostInput1);
-  free(hostInput2);
-  free(hostOutput);
+  free(hostA);
+  free(hostB);
+  free(hostC);
+
   return 0;
 }
